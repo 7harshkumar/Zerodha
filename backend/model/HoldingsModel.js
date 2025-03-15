4fd1f2794e9c96d82e1459b6c6762449751f5506
Harsh Kumar <221220025@nitdelhi.ac.in>
2025-03-15 23:38:24 +0530

    REPO

--- backend/model/HoldingsModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const HoldingsSchema = require("../schemas/HoldingsSchema"); // ✅ Import schema directly
-
-const HoldingsModel = mongoose.model("Holding", HoldingsSchema); // ✅ Correct Model Definition
-
-module.exports = HoldingsModel; // ✅ Correct Export
